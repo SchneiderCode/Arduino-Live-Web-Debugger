08441f9dbb7583daf0cb39e2e53762eb362509a6
Michael Joseph Schneider <misc7570@colorado.edu>
2020-09-18 11:39:37 -0600

    New Demo Sept 18th

---
+++ precache-manifest.6415a331abcf3e6fc150a92f5415bfe7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85af258907acd1d5f66ecbfd7d8d7e2f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61b7d1131dec0227a1c8",
+    "url": "./static/css/main.52c77f81.chunk.css"
+  },
+  {
+    "revision": "1daa8162d4d47cfea875",
+    "url": "./static/js/0.31634adb.chunk.js"
+  },
+  {
+    "revision": "db414ea89f5bb44bbcfe",
+    "url": "./static/js/1.fafbac7e.chunk.js"
+  },
+  {
+    "revision": "e7b19f3055e72ea447f3",
+    "url": "./static/js/4.a3123495.chunk.js"
+  },
+  {
+    "revision": "d9eaf7e8dea1b5a94369351518ab2d97",
+    "url": "./static/js/4.a3123495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f42ec974c88f6d85a0",
+    "url": "./static/js/5.225ac91f.chunk.js"
+  },
+  {
+    "revision": "e240cc0f81423a82a060",
+    "url": "./static/js/6.4f43e9d2.chunk.js"
+  },
+  {
+    "revision": "cda6f2748452b5c63703",
+    "url": "./static/js/7.8b3f85cb.chunk.js"
+  },
+  {
+    "revision": "b92b15210877d4cdbb9a",
+    "url": "./static/js/8.e06faf84.chunk.js"
+  },
+  {
+    "revision": "61b7d1131dec0227a1c8",
+    "url": "./static/js/main.34477f7e.chunk.js"
+  },
+  {
+    "revision": "ae52a5cb766bc5d3c710",
+    "url": "./static/js/runtime-main.d8fcc95a.js"
+  }
+]);
